3e2bf0be7873a302486d2e76bffbd1e9eb899469
Minin <70429604+mininxd@users.noreply.github.com>
2024-09-24 10:53:13 +0700

    Update promptGen.js

--- js/promptGen.js
+++ js/promptGen.js
@@ -8,23 +8,11 @@ import {
 
 import {AiAuth} from "./a.js";
 export function getRandPrompt() {
-  fetch('https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=' + AiAuth, 
-{
-  method: 'POST',
-   body: JSON.stringify({"prompt": 
-      { "text": "Write a random good image prompt"} 
-   }),
-   
-   headers: {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json',
-  }
-  
-}).then((res) => {
+  fetch("https://api.mininxd.my.id/gemini/?q= make an image generator prompt, only prompt").then((res) => {
   return res.json();
 }).then((data) => {
-    // console.log(data.candidates[0].output);
-  input.value = JSON.stringify(data.candidates[0].output).toString().replace(/"/g,"");
+    // console.log(data.text);
+  input.value = JSON.stringify(data.text).toString().replace(/"/g,"");
   promptGenBtnRan.disabled = false;
   promptGenInput.disabled = false;
   loadRand.style.display = "none";
@@ -38,23 +26,11 @@ export function getRandPrompt() {
 
 export function getPrompt() {
  
-  fetch('https://generativelanguage.googleapis.com/v1beta3/models/text-bison-001:generateText?key=' + AiAuth, 
-{
-  method: 'POST',
-   body: JSON.stringify({"prompt": 
-      { "text": 'Make image prompt of "' + promptGenInput.value + '"'} 
-   }),
-   
-   headers: {
-  'Accept': 'application/json',
-  'Content-Type': 'application/json',
-  }
-  
-}).then((res) => {
+  fetch(`https://api.mininxd.my.id/gemini/?q=make an prompt of ${promptGenInput.value}, image prompt, and only prompt`).then((res) => {
   return res.json();
 }).then((data) => {
-  // console.log(data.candidates[0].output);
-  input.value = JSON.stringify(data.candidates[0].output).toString().replace(/"/g,"");
+  // console.log(data.text);
+  input.value = JSON.stringify(data.text).toString().replace(/"/g,"");
   promptGenBtn.disabled = false;
   promptGenInput.disabled = false;
   loadGen.style.display = "none";
